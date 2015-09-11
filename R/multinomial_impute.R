d055ef57922ff93c2792b778d3621b7ac814bf35
Alex Whitworth <awhitworth@dandb.com>
2015-09-11 13:57:10 -0700

    S3 typo --> S4

--- R/multinomial_impute.R
+++ R/multinomial_impute.R
@@ -87,7 +87,7 @@ multinomial_impute <- function(dat, method= c("EM", "DA"),
   # 04. Impute missing values 
   #----------------------------------------------
   # EM & DA
-  dat_miss2 <- impute_multinomial_all(dat_miss, mle_multinomial$MLEx_y)
+  dat_miss2 <- impute_multinomial_all(dat_miss, mle_multinomial@mle_x_y)
   
   #combine:
   imputed_data <- unique(rbind(dat_comp, dat_miss2))
